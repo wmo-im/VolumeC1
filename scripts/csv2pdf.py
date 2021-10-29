7f207df0bb904465e3851b63863fbe08d81c2e92
xchen <58480226+chenxiaoxia2019@users.noreply.github.com>
2021-10-29 10:04:16 +0200

    Update csv2pdf.py

--- scripts/csv2pdf.py
+++ scripts/csv2pdf.py
@@ -13,32 +13,32 @@ with open('info/CCCC_en.csv', newline='') as f:
     for row in reader:
         print(row)
         
-pdf = FPDF()
-	pdf.add_page()
-	page_width = pdf.w - 2 * pdf.l_margin
+        pdf = FPDF()
+pdf.add_page()
+page_width = pdf.w - 2 * pdf.l_margin
 		
-	pdf.set_font('Times','B',14.0) 
-	pdf.cell(page_width, 0.0, 'CCCC List', align='C')
-	pdf.ln(10)
+pdf.set_font('Times','B',14.0) 
+pdf.cell(page_width, 0.0, 'CCCC List', align='C')
+pdf.ln(10)
 
-	pdf.set_font('Courier', '', 12)
+pdf.set_font('Courier', '', 12)
 	
-	col_width = page_width/3
+col_width = page_width/3
 	
-	pdf.ln(1)
+pdf.ln(1)
 	
-	th = pdf.font_size
+th = pdf.font_size
 	
-	for row in reader:
+for row in reader:
         #print(row)
-		pdf.cell(col_width, th, str(row[0]), border=1)
-		pdf.cell(col_width, th, row[1], border=1)
-		pdf.cell(col_width, th, row[2], border=1)
-		pdf.ln(th)
+    pdf.cell(col_width, th, str(row[0]), border=1)
+    pdf.cell(col_width, th, row[1], border=1)
+    pdf.cell(col_width, th, row[2], border=1)
+    pdf.ln(th)
 		
-	pdf.ln(10)
+pdf.ln(10)
 
-	pdf.set_font('Times','',10.0) 
-	pdf.cell(page_width, 0.0, '- end of report -', align='C')
+pdf.set_font('Times','',10.0) 
+pdf.cell(page_width, 0.0, '- end of report -', align='C')
 	
-	pdf.output('CCCC_en.pdf', 'F')
+pdf.output('CCCC_en.pdf', 'F')
